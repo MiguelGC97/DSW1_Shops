0c38c4963d781dbf32eeec5d054296ed8830c935
MiguelGC97 <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2024-09-23 09:53:08 +0100

    POST created

--- frontend/src/app/services/shop.service.ts
+++ frontend/src/app/services/shop.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +12,21 @@ export class ShopService {
 
   getShops() {
     return this.httpClient.get(this.endpoint);
+  }
+
+  /*create(shopData: any) {
+    return this.httpClient.post(this.endpoint, shopData);
+  }*/
+
+  create(shopData: any){
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+
+    const body = new URLSearchParams();
+    body.append("address", shopData.address);
+    body.append("telephone", shopData.telephone);
+
+    return this.httpClient.post(this.endpoint, body.toString(), { headers });
   }
 }
--- frontend/src/app/shop-form/shop-form.page.ts
+++ frontend/src/app/shop-form/shop-form.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../services/shop.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shop-form',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopFormPage implements OnInit {
 
-  constructor() { }
+  shopForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder,
+    private shopService: ShopService,
+    private route: Router) {
+      this.shopForm = this.formBuilder.group({
+        address: ['', Validators.compose([Validators.required])],
+        telephone: ['', Validators.compose([Validators.required])],
+      })
+    }
 
   ngOnInit() {
   }
 
+  createShop() {
+    if (this.shopForm.valid) {
+      console.log('Valid form:', this.shopForm.value);
+      this.shopService.create(this.shopForm.value).subscribe(response => {
+        this.route.navigateByUrl("/shop");
+      })
+    } else {
+      console.log('Form not valid');
+    }
+  }
+
+  getFormControl(field: string) {
+    return this.shopForm.get(field);
+  }
 }
